10b352b884351b77f23b3565d1d3d87d180e413e
guiiwty <143542936+guiiwty@users.noreply.github.com>
2024-09-27 15:08:51 -0300

    Add files via upload

---
+++ q06.py
@@ -0,0 +1,7 @@
+dinheiro_usuario = float(input("Quanto dinheiro você possui? "))
+
+quantidade_litros = dinheiro_usuario / 5
+
+quantidade_km = quantidade_litros * 20
+
+print(f"Você consegue comprar {quantidade_litros:.1f}L e conseguirá andar por {quantidade_km:.1f} km.")
