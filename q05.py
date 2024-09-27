b2786af950eccfaabad7d025374decbc152015eb
guiiwty <143542936+guiiwty@users.noreply.github.com>
2024-09-27 15:08:34 -0300

    Add files via upload

---
+++ q05.py
@@ -0,0 +1,13 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo número: "))
+num3 = float(input("Informe o terceiro número: "))
+
+produto1 = (num1*2) + (num2/2)
+
+produto2 = (num1*3) + num3
+
+produto3 = num3 ** 3
+
+print(f"O dobro do primeiro número com a metade do segundo número é: {produto1} .")
+print(f"O triplo do primeiro número com o terceiro número é: {produto2} .")
+print(f"O terceiro número ao cubo é: {produto3} .")
