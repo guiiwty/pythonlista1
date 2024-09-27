309d3388f300224365908b1e178ced7b7d9c916a
guiiwty <143542936+guiiwty@users.noreply.github.com>
2024-09-27 15:08:16 -0300

    Add files via upload

---
+++ q04.py
@@ -0,0 +1,5 @@
+temperatura_f = float(input("Qual a temperatura em fahrenheit? "))
+
+temperatura_c = 5 * ((temperatura_f - 32) / 9)
+
+print(f"A sua temperatura de fahrenheit para celsius é: {temperatura_c}°C")
