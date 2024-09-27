fe6084b19574db9bdc0595999c2347717af8625a
guiiwty <143542936+guiiwty@users.noreply.github.com>
2024-09-27 15:07:42 -0300

    Add files via upload

---
+++ q02.py
@@ -0,0 +1,7 @@
+ano_nascimento = int(input("Qual seu ano de nascimento? "))
+
+ano_atual = int(input("Em que ano você estamos? "))
+
+idade_atual = ano_atual - ano_nascimento
+
+print(f"A sua idade é: {idade_atual}")
