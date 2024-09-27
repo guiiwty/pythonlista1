69c5bbb56c8d319169a4e661ffeeb390767a2c2b
guiiwty <143542936+guiiwty@users.noreply.github.com>
2024-09-27 15:06:31 -0300

    q01.py

---
+++ q01.py
@@ -0,0 +1,7 @@
+ganho_por_hora = float(input("Quanto você ganha por hora? R$: "))
+
+horas_trabalhadas = float(input("Quantas horas você trabalhou no mês? "))
+
+salario_total = ganho_por_hora * horas_trabalhadas
+
+print(f"O seu salário total no mês é: R$ {salario_total:.2f}")
